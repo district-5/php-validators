a611078b8dbacbfa7846a913f4be9559ebc025c7
Mark Morgan <mark.morgan@district5.co.uk>
2024-09-03 10:04:20 +0100

    added unit tests for string length

---
+++ tests/ValidatorsTests/StringLengthTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace District5Tests\ValidatorsTests;
+
+use District5\Validators\StringLength;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StringLengthTest
+ * @package District5\ValidatorTests\Adapters
+ */
+class StringLengthTest extends TestCase
+{
+    public function testValidExactValue()
+    {
+        $instance = new StringLength(['exact' => 4]);
+
+        $this->assertTrue($instance->isValid('test'));
+        $this->assertNull($instance->getLastErrorMessageKey());
+    }
+
+    public function testValidInRangeValue()
+    {
+        $instance = new StringLength(['min' => 4, 'max' => 6]);
+
+        $this->assertTrue($instance->isValid('hello'));
+        $this->assertNull($instance->getLastErrorMessageKey());
+    }
+
+    public function testInvalidExactNull()
+    {
+        $instance = new StringLength(['exact' => 4]);
+
+        $this->assertFalse($instance->isValid(null));
+    }
+
+    public function testInvalidInRangeNull()
+    {
+        $instance = new StringLength(['min' => 4, 'max' => 6]);
+
+        $this->assertFalse($instance->isValid(null));
+    }
+
+    public function testInvalidExactTooLongLength()
+    {
+        $instance = new StringLength(['exact' => 4]);
+
+        $this->assertFalse($instance->isValid('hello'));
+        $this->assertEquals('tooLong', $instance->getLastErrorMessageKey());
+    }
+
+    public function testInvalidExactTooShortLength()
+    {
+        $instance = new StringLength(['exact' => 4]);
+
+        $this->assertFalse($instance->isValid('hi'));
+        $this->assertEquals('tooShort', $instance->getLastErrorMessageKey());
+    }
+
+    public function testInvalidInRangeTooLongLength()
+    {
+        $instance = new StringLength(['min' => 4, 'max' => 6]);
+
+        $this->assertFalse($instance->isValid('helloworld'));
+        $this->assertEquals('tooLong', $instance->getLastErrorMessageKey());
+    }
+
+    public function testInvalidInRangeTooShortLength()
+    {
+        $instance = new StringLength(['min' => 4, 'max' => 6]);
+
+        $this->assertFalse($instance->isValid('hi'));
+        $this->assertEquals('tooShort', $instance->getLastErrorMessageKey());
+    }
+}
